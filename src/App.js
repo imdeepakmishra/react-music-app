a3d3844a92d61d802212573171158d17a07c5cf6
imdeepakmishra <deepakmishra3813@gmail.com>
2020-12-02 12:56:26 +0530

    Intial styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import "./styles/app.scss";
 import Song from "./components/Song";
 import Player from "./components/Player";
 
--- src/components/Player.js
+++ src/components/Player.js
@@ -8,28 +8,28 @@ import {
 const Player = () => {
   return (
     <div className="player">
-      <h1>Player Component</h1>
       <div className="time-control">
         <p>Start Time</p>
         <input type="range" />
         <p> End Time</p>
       </div>
-      <div className="play-control"></div>
-      <FontAwesomeIcon
-        className="skip-back"
-        icon={faAngleLeft}
-        size="2x"
-      ></FontAwesomeIcon>
-      <FontAwesomeIcon
-        className="play"
-        icon={faPlay}
-        size="2x"
-      ></FontAwesomeIcon>
-      <FontAwesomeIcon
-        className="skip-forward"
-        icon={faAngleRight}
-        size="2x"
-      ></FontAwesomeIcon>
+      <div className="play-control">
+        <FontAwesomeIcon
+          className="skip-back"
+          icon={faAngleLeft}
+          size="2x"
+        ></FontAwesomeIcon>
+        <FontAwesomeIcon
+          className="play"
+          icon={faPlay}
+          size="2x"
+        ></FontAwesomeIcon>
+        <FontAwesomeIcon
+          className="skip-forward"
+          icon={faAngleRight}
+          size="2x"
+        ></FontAwesomeIcon>
+      </div>
     </div>
   );
 };
